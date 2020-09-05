8618e6a6dde6fdf0d47f829de92cce8b6ca1e1a6
zurirma2020 <58671073+zurirma2020@users.noreply.github.com>
2020-09-05 13:27:57 -0300

    json placeholder added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css';
 
 
@@ -13,11 +12,17 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
-
+/*updating the state*/
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users}));
+        
+    }
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
         
@@ -27,6 +32,9 @@ class App extends Component {
            const filteredRobots = this.state.robots.filter(robots => {
                 return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
             })
+            if (this.state.robots.length === 0) {
+                return <h1>Loading</h1>
+            } else {
             return (
             <div className='tc'>
             <h1 classname='f2'>Robofriends</h1>
@@ -37,6 +45,6 @@ class App extends Component {
 
     }
     
-}
+}}
 
 export default App;
